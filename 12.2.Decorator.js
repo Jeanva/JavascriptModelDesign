2f557071574b6a7c415b70cd042d0b5b331d1e8c
Jeanva <591975433@qq.com>
2019-07-02 22:47:28 +0800

    Decorator Modal updated

--- 12.2.Decorator.js
+++ 12.2.Decorator.js
@@ -19,13 +19,13 @@ var decorator = function(input,fn){
 }
 // 电话输入框功能装饰
 decorator('tel_input',function(){
-    document.getElementById('tel_input').style.display='none';
+    document.getElementById('tel_demo_input').style.display='none';
 });
 // 姓名输入框功能装饰
 decorator('name_input',function(){
-    document.getElementById('name_input').style.display='none';
+    document.getElementById('name_demo_input').style.display='none';
 });
 // 地址输入框功能装饰
 decorator('address_input',function(){
-    document.getElementById('address_input').style.display='none';
+    document.getElementById('address_demo_input').style.display='none';
 })
