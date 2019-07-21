13d69579eda006dc0e67f178ccf45718f13b2da0
Jeanva <591975433@qq.com>
2019-07-21 22:26:16 +0800

    Modal Strategy modified

--- 19.6.Strategy.js
+++ 19.6.Strategy.js
@@ -5,10 +5,10 @@ var InputStrategy = function(){
             return /\s+/.test(value) ? console.log('请输入内容') : '';
         },
         number : function(value){// 是否是一个数字
-            return /^[0-9]+(\.[0-9]+)?$/.test(value) ? console.log(value) :console.log('请输入数字');
+            return /^[0-9]+(\.[0-9]+)?$/.test(value) ? '' :'请输入数字';
         },
         phone : function(value){// 是否是本地电话
-            return /^\d[3]\-\d{8}$|^\d{4}\-\d{7}$/.test(value) ? console.log(value) :console.log('请输入正确的电话好吗格式，如：010-12345678 ');
+            return /^\d[3]\-\d{8}$|^\d{4}\-\d{7}$/.test(value) ? '':'请输入正确的电话好吗格式，如：010-12345678 ';
         }
     }
     return {
@@ -16,7 +16,7 @@ var InputStrategy = function(){
         check : function(type, value){  
             // 去除收尾空白符
             value = value.replace(/^\s+|\s+$/g,'');
-            return strategy[type] ? strategy[type](value): console.log('没有该类型的检测方法')
+            return strategy[type] ? strategy[type](value):'没有该类型的检测方法'
         },
         // 添加策略
         addStrategy : function(type,fn){
